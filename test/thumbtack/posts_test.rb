59b3c23c2ce48362d77e9dc4f563bbf44ed36196
Nate Smith <nate.smith@jadedpixel.com>
2014-06-28 12:53:56 -0400

    posts/add

--- test/thumbtack/posts_test.rb
+++ test/thumbtack/posts_test.rb
@@ -10,5 +10,33 @@ class PostsTest < Minitest::Test
     posts = Posts.new(client)
 
     assert_equal '2014-06-26T19:01:33Z', posts.update
+    client.verify
+  end
+
+  def test_add
+    client = Minitest::Mock.new
+    response = { 'result_code' => 'done' }
+    client.expect(:get, response,
+                  ['/posts/add',
+                   { url: 'http://example.org', description: 'example.org' }])
+    posts = Posts.new(client)
+
+    assert_equal posts, posts.add('http://example.org', 'example.org')
+    client.verify
+  end
+
+  def test_add_with_tags
+    client = Minitest::Mock.new
+    response = { 'result_code' => 'done' }
+    client.expect(:get, response,
+                  ['/posts/add',
+                   {url: 'http://example.org',
+                    description: 'example.org',
+                    tags: 'thumbtack test' }])
+    posts = Posts.new(client)
+
+    assert_equal posts,
+      posts.add('http://example.org', 'example.org', tags: 'thumbtack test')
+    client.verify
   end
 end
