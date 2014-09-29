7a165126631b9d93716d5fffe285fea5bc4db011
Nate Smith <nate.smith@shopify.com>
2014-09-29 11:57:21 -0400

    YARDify Posts

--- lib/thumbtack/posts.rb
+++ lib/thumbtack/posts.rb
@@ -1,50 +1,52 @@
 # encoding: utf-8
 
 module Thumbtack
-  # Internal: Wraps API calls related to posts.
+  # Wraps API calls related to posts
   class Posts
-    # Internal: Initialize a Posts.
+    # Initialize a Posts
     #
-    # client - A Thumbtack::Client to communicate with the Pinboard API.
+    # @param [Client] client
+    #   client to communicate with the Pinboard API
+    #
+    # @api private
     def initialize(client)
       @client = client
     end
 
-    # Public: The most recent time a bookmark was added, updated, or deleted.
+    # Fetch the most recent time a bookmark was added, updated, or deleted
     #
-    # Examples
+    # @return [DateTime]
     #
-    #   update
-    #   # => '2014-06-26T19:01:33Z'
-    #
-    # Returns a DateTime in UTC.
+    # @api public
     def update
       response = @client.get('/posts/update')
       Types::DateTime.from_parameter response.fetch('update_time')
     end
 
-    # Public: Add a bookmark.
+    # Add a bookmark
     #
-    # url - A String containing the URL to be bookmarked.
-    # description - A String containing the title of the bookmark.
-    # options - The Hash options to be passed as arguments.
-    #           :extended - A String containing a description of the item.
-    #           :tags     - An Array containing up to 100 tags.
-    #           :dt       - A DateTime containing the creation time for this
-    #                       bookmark.
-    #           :replace  - A Boolean containing whether or not to replace any
-    #                       existing bookmark with this URL.
-    #           :shared   - A Boolean containing whether or not to make the
-    #                       bookmark public.
-    #           :toread   - A Boolean containing whether or not to mark the
-    #                       bookmark as unread.
+    # @param [String] url
+    #   the URL of the bookmark
+    # @param [String] description
+    #   title of the bookmark
+    # @param [Hash] options
+    #   options for the bookmark addition
+    # @option options [String] :extended
+    #   a description of the bookmark
+    # @option options [Array[String]] :tags
+    #   a list of up to 100 tags
+    # @option options [DateTime] :dt
+    #   the creation time for this bookmark
+    # @option options [Boolean] :replace
+    #   if true, replace any existing bookmark with the same URL
+    # @option options [Boolean] :shared
+    #   if true, make the bookmark public
+    # @option options [Boolean] :toread
+    #   if true, mark the bookmark unread
     #
-    # Examples
+    # @return [self]
     #
-    #   add('http://theinternate.com', 'The Internate')
-    #   add('http://theinternate.com', 'The Internate', tags: ['best', 'ruby'])
-    #
-    # Returns the Posts instance.
+    # @api public
     def add(url, description, options = EMPTY_HASH)
       parameters = Specification.new(
         url: Types::URL,
@@ -60,37 +62,36 @@ module Thumbtack
       self
     end
 
-    # Public: Delete a bookmark.
+    # Delete a bookmark
     #
-    # url - A String containing the URL of the bookmark to delete.
+    # @param [String] url
+    #   the URL of the bookmark to delete
     #
-    # Examples
+    # @return [self]
     #
-    #   delete('http://delicio.us')
-    #
-    # Returns the Posts instance
+    # @api public
     def delete(url)
       parameters = Specification.new(url: Types::URL).parameters(url: url)
       @client.get('/posts/delete', parameters)
       self
     end
 
-    # Public: Return one or more posts matching the arguments.
+    # Fetch one or more bookmarks
     #
-    # options - The Hash options to be passed as arguments.
-    #          :tag  - An Array containing a up to three tags to filter by.
-    #          :dt   - A DateTime containing the date when the results were
-    #                  bookmarked.
-    #          :url  - A String containing the URL for the the bookmark.
-    #          :meta - A Boolean indicating whether or not to include a change
-    #                  detection signature.
+    # @param [Hash] options
+    #   options to filter the results by
+    # @option options [Array<String>] :tag
+    #   up to three tags to filter by
+    # @option options [DateTime] :dt
+    #   which day the results were bookmarked
+    # @option options [String] :url
+    #   the URL for this bookmark
+    # @option options [Boolean] :meta
+    #   if true, include the change detection signature in the results
     #
-    # Examples
+    # @return [Array<Post>]
     #
-    #   get(tag: 'webdev', meta: 'yes')
-    #   get(url: 'http://www.pinboard.in')
-    #
-    # Returns a list of Post instances.
+    # @api public
     def get(options = EMPTY_HASH)
       parameters = Specification.new(
         tag: Types::Tags,
@@ -101,18 +102,18 @@ module Thumbtack
       posts_from @client.get('/posts/get', parameters)
     end
 
-    # Public: Return a list of the most recent posts.
+    # List the most recent bookmarks
     #
-    # options - The Hash options to be passed as arguments.
-    #          :tag   - An Array containing a up to three tags to filter by.
-    #          :count - An Integer of the number of results to return. Default
-    #                   is 15, maximum is 100.
+    # @param [Hash] options
+    #   options to filter the results by
+    # @option options [Array<String>] :tag
+    #   up to three tags to filter by
+    # @option options [Integer] :count
+    #   the number of results to return
     #
-    # Examples
+    # @return [Array<Post>]
     #
-    #   recent(tag: 'webdev', count: 25)
-    #
-    # Returns a list of Post instances.
+    # @api public
     def recent(options = EMPTY_HASH)
       parameters = Specification.new(
         tag: Types::Tags,
@@ -121,19 +122,26 @@ module Thumbtack
       posts_from @client.get('/posts/recent', parameters)
     end
 
-    # Public: Return all posts in the account.
+    # List all bookmarks
     #
-    # options - The Hash options to be passed as arguments.
-    #          :tag     - A String containing a list of up to three tags to
-    #                     filter by.
-    #          :start   - An Integer offset value. Default is 0.
-    #          :results - An Integer of the number of results to return.
-    #          :fromdt  - Filter results to posts created after this DateTime.
-    #          :todt    - Filter results to posts created before this DateTime.
-    #          :meta    - A Boolean containing whether or not to include a
-    #                     change detection signature.
+    # @param [Hash] options
+    #   options to filter the results by
+    # @option options [Array<String>] :tag
+    #   up to three tags to filter by
+    # @option options [Array<String>] :start
+    #   an offset value
+    # @option options [Array<String>] :results
+    #   number of results to return
+    # @option options [DateTime] :fromdt
+    #   limit results to those created after this time
+    # @option options [DateTime] :todt
+    #   limit results to those created before this time
+    # @option options [Boolean] :meta
+    #   if true, include the change detection signature in the results
     #
-    # Returns a list of Posts instances
+    # @return [Array<Post>]
+    #
+    # @api public
     def all(options = EMPTY_HASH)
       parameters = Specification.new(
         tag: Types::Tags,
@@ -147,12 +155,22 @@ module Thumbtack
       results.map { |post_hash| Post.from_hash(post_hash) }
     end
 
-    # Public: Return a list of popular and recommended tags for a URL.
+    # List popular and recommended tags for a URL
     #
-    # url - A String containing a URL to fetch suggested tags for.
+    # @param [String] url
+    #   URL to fetch suggested tags for
     #
     # Returns a Hash with two entries, :popular is a list of popular tags,
     # :recommended is a list of recommended tags.
+    #
+    # @return [Hash<Symbol => Array<String>>]
+    #   the hash has two entries:
+    #   [:popular] a list of popular tags
+    #   [:recommended] a list of recommended tags
+    #
+    # @todo this should return an object, not a Hash
+    #
+    # @api public
     def suggest(url)
       parameters = Specification.new(url: Types::URL).parameters(url: url)
       result = @client.get('/posts/suggest', parameters)
@@ -160,13 +178,18 @@ module Thumbtack
         recommended: result.fetch('recommended') }
     end
 
-    # Public: Return a list dates with the count of posts made at each date.
+    # List dates with the number of bookmarks created on each
     #
-    # options - The Hash options to be passed as arguments
-    #          :tag - An Array containing a up to three tags to filter by.
+    # @param [Hash] options
+    #   options to filter the results by
+    # @option options [Array<String>] :tag
+    #   up to three tags to filter by
     #
-    # Returns a Hash of Strings => Integer entries. The Strings contain a date
-    # and the Integer is the count of posts made on that date.
+    # @return [Hash<Date => Integer>]
+    #   dates on which bookmarks were created, and the number of bookmarks
+    #   created on that date
+    #
+    # @api public
     def dates(options = EMPTY_HASH)
       parameters = Specification.new(tag: Types::Tags).parameters(options)
       response = @client.get('/posts/dates', parameters)
