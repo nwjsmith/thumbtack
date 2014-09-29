6ecc03ff16db6a8635a12622412ed1ade6ce78cb
Nate Smith <nwjsmith@gmail.com>
2014-09-29 10:55:46 -0400

    YARDify Notes

--- lib/thumbtack/notes.rb
+++ lib/thumbtack/notes.rb
@@ -1,16 +1,23 @@
 # encoding: utf-8
 
 module Thumbtack
-  # Internal: Wraps API calls related to notes.
+  # Wraps API calls related to notes
   class Notes
-    # Internal: Creates a new Notes.
+    # Initialize a Notes
     #
-    # client - a Thumbtack::Client to communicate with the Pinboard API.
+    # @param [Client] client
+    #   client to communicate with the Pinboard API
+    #
+    # @api private
     def initialize(client)
       @client = client
     end
 
-    # Public: Returns a list of summaries of the user's notes.
+    # List of summaries of the user's notes
+    #
+    # @return [Array<NoteSummary>]
+    #
+    # @api public
     def list
       response = @client.get('/notes/list')
       response.fetch('notes', []).map do |note_hash|
@@ -18,7 +25,14 @@ module Thumbtack
       end
     end
 
-    # Public: Return an individual note.
+    # Fetch a note
+    #
+    # @param [#to_s] id
+    #   the id of the note to fetch
+    #
+    # @return [Note]
+    #
+    # @api public
     def get(id)
       Note.from_hash @client.get("/notes/#{id}")
     end
