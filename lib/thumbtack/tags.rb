1dd3440f7066ebc00acb78571742e45f3692a7e6
Nate Smith <nate.smith@shopify.com>
2014-09-29 12:02:37 -0400

    YARDify Tags

--- lib/thumbtack/tags.rb
+++ lib/thumbtack/tags.rb
@@ -1,38 +1,53 @@
 # encoding: utf-8
 
 module Thumbtack
-  # Internal: Wraps API calls related to tags.
+  # Wraps API calls related to tags
   class Tags
-    # Internal: Creates a new Tags.
+    # Initialize a Tags
     #
-    # client - a Thumbtack::Client to communicate with the Pinboard API.
+    # @param [Client] client
+    #   client to communicate with the Pinboard API
+    #
+    # @api private
     def initialize(client)
       @client = client
     end
 
-    # Public: Returns a list of the user's tags with their counts.
+    # List tags with their counts
+    #
+    # @return [Hash<String => Integer>]
+    #   tags associated with the number of times they have been used
+    #
+    # @api public
     def get
       response = @client.get('/tags/get')
       Hash[response.map { |tag, count| [tag, count.to_i] }]
     end
 
-    # Public: Delete a tag.
+    # Delete a tag
     #
-    # tag - A String containing the tag to delete.
+    # @param [String] tag
+    #   the tag to delete
     #
-    # Returns the Tags instance.
+    # @return [self]
+    #
+    # @api public
     def delete(tag)
       parameters = Specification.new(tag: Types::Tags).parameters(tag: tag)
       @client.get('/tags/delete', parameters)
       self
     end
 
-    # Public: Rename a tag.
+    # Rename a tag
     #
-    # old - A String containing the tag to be renamed.
-    # new - A String containing the new name for the tag.
+    # @param [String] old
+    #   the tag to be renamed
+    # @param [String] new
+    #   the new name for the tag
     #
-    # Returns the Tags instance.
+    # @return [self]
+    #
+    # @api public
     def rename(old, new)
       parameters = Specification.new(
         old: Types::Tags,
