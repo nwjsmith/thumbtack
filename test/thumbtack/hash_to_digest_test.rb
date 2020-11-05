ea40bae603eea2240de5c07eab65c12dd05ef40d
Nate Smith <nwjsmith@gmail.com>
2020-11-05 10:43:42 -0500

    Add unit test for `HashToDigest.rename`

---
+++ test/thumbtack/hash_to_digest_test.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class HashToDigestTest < Minitest::Test
+  def test_rename
+    assert_equal(
+      {
+        'digest' => 'value', 'other' => 'value'
+      },
+      HashToDigest.rename('hash' => 'value', 'other' => 'value')
+    )
+  end
+end
